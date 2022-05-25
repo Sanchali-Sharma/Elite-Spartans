096d85a229dc764eae5278e6037afae4cca18d6f
sanchali <sharmasanchali06@gmail.com>
2022-05-26 00:31:30 +0530

    sanchali
    audio play

--- devjamjs.js
+++ devjamjs.js
@@ -1,43 +1,125 @@
-console.log("hello");
-let index=0;
-let audioItem = new Audio('OSANAM.mp3');
-let playpause=document.getElementById('play');
-let songbar=document.getElementById('bar');
-let songs=[
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-    {songname:"", path:"",cover:"" },
-]
-playpause.addEventListener('click',()=>{
-    if(audioItem.paused||audioItem.currentTime<=0){
-        audioItem.play();
-        playpause.classList.remove('fa-play');
-        playpause.classList.add('fa-pause');
-    }
-    else{
-        audioItem.pause();
-        playpause.classList.remove('fa-pause');
-        playpause.classList.add('fa-play');
-    }
-})
-audioItem.addEventListener('timeupdate',()=>{
-    prog=parseInt((audioItem.currentTime/audioItem.duration)*100);
-    songbar.value=prog;
-})
-songbar.addEventListener('change',()=>{
-    audioItem.pause();
-    audioItem.currentTime=(songbar.value*audioItem.duration)/100 ;
-    audioItem.play();
-})
+function function1() {
+   var x= document.getElementById('song')
+   x.src = "http://docs.google.com/uc?export=open&id=1AOiUueJf2qDBIlzrAWF_vutwo-wn3ivN";
+   x.play();
+}
+function function2() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function3() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function4() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function5() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function6() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function7() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function8() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function9() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function10() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function11() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function12() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function13() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function14() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function15() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function16() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function17() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function18() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function19() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function20() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function21() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function22() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function23() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function24() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
+ function function25() {
+    var x= document.getElementById('song')
+    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.play();
+ }
