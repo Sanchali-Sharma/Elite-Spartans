e87d8d63dc7148ce599243ac204995d45c3f18c1
sanchali <sharmasanchali06@gmail.com>
2022-05-26 01:43:07 +0530

    sanchali
    all audio done

--- devjamjs.js
+++ devjamjs.js
@@ -5,121 +5,121 @@ function function1() {
 }
 function function2() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1sqXpuAms5gd4DS30HxTpS5DuiQnzERS0";
     x.play();
  }
  function function3() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1uxnSHX2O5fnd2IcGSAOf7plm5xqv4daf";
     x.play();
  }
  function function4() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1eE8APxlfPS-vBIrgkwMsgKgmeavYoJf3";
     x.play();
  }
  function function5() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1kQWgB31mZIyy3TsWkNlraB38LL96AFYc";
     x.play();
  }
  function function6() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1lT64CY5iJg5og2kDK4PTO7EgLuFW_owr";
     x.play();
  }
  function function7() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1Yaq91HUcF6RE_FNJ2WeNO68X9AlAOush";
     x.play();
  }
  function function8() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1XGGzRS6DPXekAxqZphsqewCfxPt07M6U";
     x.play();
  }
  function function9() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=15nMuf6fl-iP6RSf2oWqHI9YGMWDTsqqe";
     x.play();
  }
  function function10() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1bu8f0D91VhelbC-UFfrq5Z_g8LU6FApE";
     x.play();
  }
  function function11() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1WFUeHY8oKY7JG_VSi0b9oMdH9bHRgDa8";
     x.play();
  }
  function function12() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1HK8UDUr0GRFkn8PaZp_bm2mGCpTQ6q2s";
     x.play();
  }
  function function13() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1LDTZOdlPVF6Ala6EOqWqcbBkSZM15AuA";
     x.play();
  }
  function function14() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1j9FMEiO8LGPfQGpty8QGWrrOl_j-C5sM";
     x.play();
  }
  function function15() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=19tXrM01OzOpFCZO8H5Nz3fcII5SiecaU";
     x.play();
  }
  function function16() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=13S2x3-iuFxIOtxmoTVSb3PcRZEj4CMP4";
     x.play();
  }
  function function17() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1UeZ26eQSyILvl72GTvDXd4kpqCBGf_uQ";
     x.play();
  }
  function function18() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1bQ_DelPG9PHpW4ln20FTpwc4FI7ofIo3";
     x.play();
  }
  function function19() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1UzwgrLjw8jsTa_45-4RJbKhuziuHjr-Y";
     x.play();
  }
  function function20() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1aPSXTR7M_amiH7ZqfvgTr8OKcxDhBCQJ";
     x.play();
  }
  function function21() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1Me6-AClxJH9g-6hFH7PBVxfTfwiYryVS";
     x.play();
  }
  function function22() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1E9PcfiXV8GHl2bWXCCfzOy0f4S4SzdSW";
     x.play();
  }
  function function23() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1TmUV6__fZkFWuiW7S3agGnDtLQ7mU1vG";
     x.play();
  }
  function function24() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1FiTekHgXgw-o1Zr9iCJMVw4Y4PAj6zSu";
     x.play();
  }
  function function25() {
     var x= document.getElementById('song')
-    x.src = "http://docs.google.com/uc?export=open&id=";
+    x.src = "http://docs.google.com/uc?export=open&id=1aRohpzdSuY_e-vZYY0oLbkxgW9_qaTYE";
     x.play();
  }
